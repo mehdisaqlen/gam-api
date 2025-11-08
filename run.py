24606c3a55ba01191e76a859fd29a90cc2e91f5a
Saqlen Raza Mehdi <mehdisaqlendev@gmail.com>
2025-11-09 01:33:49 +0500

    update run.py

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 # run.py
+import os
 import uvicorn
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", "8000"))  # Render sets PORT env var
     uvicorn.run(
         "app.main:app",
-        host="127.0.0.1",
-        port=8000,
-        reload=True
+        host="0.0.0.0",      # must be 0.0.0.0 for Render
+        port=port,
+        reload=False,        # no reload in production
     )
